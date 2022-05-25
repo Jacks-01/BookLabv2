ddd2ae8368aa663d1a1c8d825716c8ffc6c3c538
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-05-25 13:45:23 -0500

    created axios get request and called it on mount

--- src/App.js
+++ src/App.js
@@ -5,3 +5,43 @@
  */
 
 import React, { Component } from 'react';
+import axios from 'axios';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      books: []
+    }
+  }
+
+  componentDidMount(){
+    this.grabBooks();
+  }
+
+  grabBooks = () => {
+    axios({
+      method: 'get',
+      url: process.env.URL_API,
+      params: null,
+
+    })
+    .then((res) => {
+      console.log(`master data for all books: ${res.data}`)
+      this.setState({books: res.data})
+    }).catch((err) => {
+      console.error(`ERROR in GET ${err}`);
+    });
+  }
+  render() { 
+    return ( 
+      <>
+        <p>Router goes here</p>
+        <p>Form goes here</p>
+        <p>List goes here</p>
+      </>
+     );
+  }
+}
+ 
+export default App;
