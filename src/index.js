bbd93060a23fc36a2dfbc49f9f595119ea0daad7
Jack Stubblefield <92601085+Jacks-01@users.noreply.github.com>
2022-06-23 12:47:33 -0500

    installed auth0

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
+import { Auth0Provider } from '@auth0/auth0-react';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<Auth0Provider>
+		<React.StrictMode
+    domain='dev-ceitl5-9.us.auth0.com'
+    clientId='OPE5cKhHEiULD1nmaQpfcCPYXoDNtUlq'
+    redirectURI={window.location.origin}>
+			<App />
+		</React.StrictMode>
+	</Auth0Provider>
 );
-
